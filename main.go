e7d5556d17923f0aa897f718555213de2b92c05d
Liedsonfsa <fabricoliedson@gmail.com>
2024-08-02 16:32:44 -0300

    carregando o template de login

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 	"log"
 	"net/http"
 	"webapp/src/router"
+	"webapp/src/utils"
 )
 
 // git config --global core.autocrlf true
 
 func main() {
 	fmt.Println("Rodando webapp")
+	utils.CarregarTemplates()
 
 	r := router.Gerar()
 	log.Fatal(http.ListenAndServe(":3000", r))

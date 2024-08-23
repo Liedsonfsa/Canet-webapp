f0889a1e60cb4c1331acb87915aea01edf7df4f6
Liedsonfsa <fabricoliedson@gmail.com>
2024-08-23 16:12:02 -0300

    feat: criando a rota para a home

---
+++ src/router/rotas/home.go
@@ -0,0 +1,14 @@
+package rotas
+
+import(
+	"net/http"
+	"webapp/src/controllers"
+)
+
+var rotaPrincipal = Rota{
+	URI:    				"/home",
+	Metodo: 				http.MethodGet,
+	Funcao: 				controllers.CarregarPaginaPrincipal,
+	RequerAutenticacao: 	true,
+}
+
